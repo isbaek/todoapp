83649e4592848bef071681162da66d2b14c829db
Inseo B <yunibaek@hotmail.com>
2017-05-19 12:59:47 +0200

    Change App to implement title and todos

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+//import VisibleTodoList from './components/ListToDo'
+import Todos from "./components/ListToDo";
+import AddTodo from "./components/AddToDo";
+import Title from "./components/Title";
+import { connect } from "react-redux";
 
-class App extends Component {
+class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <Title />
+        <Todos />
       </div>
     );
   }
