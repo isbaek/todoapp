4a094939683bff97102874e10577bbd494ee1241
Inseo B <yunibaek@hotmail.com>
2017-05-19 14:04:05 +0200

    Store: Add Redux DevTools

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
-import React from 'react'
-import { render } from 'react-dom'
-import { Provider } from 'react-redux'
-import { createStore } from 'redux'
-import todo from './reducers/todo'
-import App from './App'
-import './index.css';
+import React from "react";
+import { render } from "react-dom";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import todo from "./reducers/todo";
+import App from "./App";
+import "./index.css";
 
-
-let store = createStore(todo)
+let store = createStore(
+  todo,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById('root')
-)
+  document.getElementById("root")
+);
