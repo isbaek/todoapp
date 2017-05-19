0005b90818704c73af8d83f3e773f85de0910a00
Inseo B <yunibaek@hotmail.com>
2017-05-19 15:56:03 +0200

    Add Delete Button and dispatch action

--- src/components/ListToDo.js
+++ src/components/ListToDo.js
@@ -1,14 +1,21 @@
 import React from "react";
 import { connect } from "react-redux";
-import AddTodo from "./AddToDo";
-import { addTodo } from "../constants/actions";
+import { addTodo, deleteToDo } from "../constants/actions";
+import Faclose from "react-icons/lib/fa/close";
 
 function Todos({ todos, dispatch }) {
   return (
     <div>
       <h1>Todos</h1>
       <ul>
-        {todos.map(todo => <li key={todo.id}>#{todo.id} - {todo.text}</li>)}
+        {todos.map(todo => (
+          <li key={todo.id}>
+            {todo.text}
+            <button onClick={id => dispatch(deleteToDo(todo.id))}>
+              Delete
+            </button>
+          </li>
+        ))}
       </ul>
     </div>
   );
