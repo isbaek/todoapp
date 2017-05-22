542024a8e00410b7b7d559c01a7a6f0259953dcc
Inseo B <yunibaek@hotmail.com>
2017-05-22 15:32:40 +0200

    Styling for Title + Add To DO form

--- src/components/AddToDo.js
+++ src/components/AddToDo.js
@@ -1,13 +1,18 @@
 import React from "react";
 import { connect } from "react-redux";
 import { addTodo } from "../constants/actions";
+import "../stylesheets/components/Addtodo.css";
 
 let input;
 
 function AddTodo({ dispatch }) {
   return (
     <div>
+      <div className="welcome center">
+        <h2>Welcome! Please add a to do</h2>
+      </div>
       <form
+        className="form"
         onSubmit={e => {
           e.preventDefault();
           if (!input.value.trim()) {
@@ -18,12 +23,15 @@ function AddTodo({ dispatch }) {
         }}
       >
         <input
+          className="input"
           ref={node => {
             input = node;
           }}
+          type="text"
+          placeholder="Type something..."
         />
-        <button type="submit">
-          Add Todo
+        <button className="button" type="submit">
+          Add To do
         </button>
       </form>
     </div>
--- src/components/Title.js
+++ src/components/Title.js
@@ -2,7 +2,13 @@ import React from "react";
 import "../stylesheets/components/Title.css";
 
 function Title() {
-  return <div className="Title"> <h1>This is a simple To Do App</h1></div>;
+  return (
+    <div className="title-container">
+      <div className="title">
+        {" "}<h1>This is a simple To Do App</h1>
+      </div>
+    </div>
+  );
 }
 
 export default Title;
